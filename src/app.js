7862a03595e160c4c229ae7d48a4af4d2ccc099d
Juan Diego Elissalde Gudefín <118232153+Juan-Diego-E@users.noreply.github.com>
2024-05-22 19:38:27 +0000

    Creando generador de excusas ejercicio en 4Geeks

--- src/app.js
+++ src/app.js
@@ -7,5 +7,27 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let p = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generator() {
+    let a = who[Math.floor(Math.random() * who.length)];
+    let b = action[Math.floor(Math.random() * action.length)];
+    let c = what[Math.floor(Math.random() * what.length)];
+    let d = when[Math.floor(Math.random() * when.length)];
+
+    return `${a} ${b} ${c} ${d}`;
+  }
+
+  p.textContent = generator();
 };
